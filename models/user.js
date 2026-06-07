90ca90f276ca5d0207523c362810268223c84fd3
hope664 <umuhozahope5@gmail.com>
2026-06-07 20:37:16 +0200

    intergration in process

--- models/user.js
+++ models/user.js
@@ -34,10 +34,10 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+// ✅ No next() needed with async pre hooks in Mongoose 7+
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 userSchema.methods.comparePassword = async function (enteredPassword) {
