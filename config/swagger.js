25ac5badb1b79fa26b2def257d3748ef2170d969
hope664 <umuhozahope5@gmail.com>
2026-06-12 17:23:05 +0200

    commented th eswagger

--- config/swagger.js
+++ config/swagger.js
@@ -1,38 +1,38 @@
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+// const swaggerJSDoc = require('swagger-jsdoc');
+// const swaggerUi = require('swagger-ui-express');
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'EATALY API',
-      version: '1.0.0',
-      description: 'Restaurant platform API'
-    },
-    servers: [
-      {
-        url: 'http://localhost:5000/api',
-        description: 'Development server'
-      }
-    ],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT'
-        }
-      }
-    },
-    security: [
-      {
-        bearerAuth: []
-      }
-    ]
-  },
-  apis: ['./routes/*.js']
-};
+// const options = {
+//   definition: {
+//     openapi: '3.0.0',
+//     info: {
+//       title: 'EATALY API',
+//       version: '1.0.0',
+//       description: 'Restaurant platform API'
+//     },
+//     servers: [
+//       {
+//         url: 'http://localhost:5000/api',
+//         description: 'Development server'
+//       }
+//     ],
+//     components: {
+//       securitySchemes: {
+//         bearerAuth: {
+//           type: 'http',
+//           scheme: 'bearer',
+//           bearerFormat: 'JWT'
+//         }
+//       }
+//     },
+//     security: [
+//       {
+//         bearerAuth: []
+//       }
+//     ]
+//   },
+//   apis: ['./routes/*.js']
+// };
 
-const swaggerSpec = swaggerJSDoc(options);
+// const swaggerSpec = swaggerJSDoc(options);
 
-module.exports = { swaggerUi, swaggerSpec };
+// module.exports = { swaggerUi, swaggerSpec };
